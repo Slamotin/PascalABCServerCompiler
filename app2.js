d58ab92d9fcc8cb054b98aedf9cf829dacedd210
Slamotin <daromir09@gmail.com>
2022-06-21 16:37:11 +0300

    Adj app2.js

--- app2.js
+++ app2.js
@@ -346,7 +346,7 @@ async function onConnect(wsClient) {
                                     wsClient.send(JSON.stringify({ action: "TASK_COMPLETE_ANSWER", data: `Тест #${checkNumber} не пройден` }));
                                     //break;
                                 };*/
-                                stdoutput == task.rows[0].testdata[iter] ? console.log('True') : console.log("false");
+                                //stdoutput == task.rows[0].testdata[iter] ? console.log('True') : console.log("false");
                                 
 
                                 console.log('stdout type: ', + typeof (stdoutput), 'isbuffer? ' + isBuffer(stdoutput) + 'isNaN ' + isNaN(stdoutput))
