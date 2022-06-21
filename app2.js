d907516c1b4964b073db1aaa3d2960ba66849233
Slamotin <daromir09@gmail.com>
2022-06-21 17:34:47 +0300

    Adj app2.js

--- app2.js
+++ app2.js
@@ -404,7 +404,7 @@ async function spawnTask(iter, task, jsonMessage, filename) {
         wsClient.send(JSON.stringify({ action: "TASK_COMPLETE_ANSWER", data: `Тест #${checkNumber} не пройден` }));
         //break;
     };*/
-    child.output[1]  == task.rows[0].testdata[iter] ? console.log('True') : console.log("false");
+    JSON.parse(child.output[1].toLowerCase()) == task.rows[0].testdata[iter] ? console.log('True') : console.log("false");
     console.log(`task #{checkNumber} uncompleted ${stdoutput} != ${task.rows[0].testdata[iter]} with ${iter}`);
 
     console.log('stdout type: ' + typeof (stdoutput) + ' output: ' + child.output[1] +  'isbuffer? ' + isBuffer(stdoutput) + 'isNaN ' + isNaN(stdoutput))
